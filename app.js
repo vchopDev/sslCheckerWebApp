70b68bbd3b2438eeefb197e3761add33f4190f20
Victor  Fernadnes <victor.martins@evoluzionetelematica.it>
2018-08-29 17:43:46 +0200

    add validation to add domain form

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var methodOverride = require('method-override')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
 
-var domainRouter = require('./routes/domains')
-var certificateRouter = require('./routes/certificates')
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/SSLChecker')
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
+var domainRouter = require("./routes/domains");
+var certificateRouter = require("./routes/certificates");
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/SSLChecker")
+  .then(() => console.log("connection succesful"))
+  .catch(err => console.error(err));
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(methodOverride('_method'))
-app.use(logger('dev'));
+app.use(methodOverride("_method"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({extended : false}))
+app.use(expressValidator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret : 'SonoUnoStronz', saveUninitialized : false, resave : false}))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/domains', domainRouter)
-app.use('/certificates', certificateRouter)
+app.use("/domains", domainRouter);
+// app.use('/certificates', certificateRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,11 +44,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/domains.js
+++ routes/domains.js
@@ -3,17 +3,20 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Domain = require('../models/Domain')
 var moment = require("moment");
+var isValidDomain = require('is-valid-domain');
 const sslCertificate = require("get-ssl-certificate");
 
 router.get('/', (req, res, next) => {
   Domain.find((err, domains) =>{
     if (err) return next(err)
     res.render('domains', {title : "Websites" , domains : domains })
+    if(req.session)
+      req.session.errors = null;
   })
 })
 
 router.get('/addDomain', (req, res, next) => {
-  res.render("addDomain")
+  res.render("addDomain", {success : req.session.success, errors : req.session.errors});
 })
 
 router.get('/:id', (req, res, next) => {
@@ -24,6 +27,22 @@ router.get('/:id', (req, res, next) => {
 })
 
 router.post('/addDomain', (req, res, next) => {
+
+  req.checkBody('name', 'Domain is required').notEmpty();
+  var errors = req.validationErrors();  
+  if(!errors) {
+    errors = [];
+    if(!isValidDomain(req.body.name)) {
+      errors.push({ param : 'name', msg : "It's not a valid Url", value : req.body.name})
+    }
+  }
+
+  if(errors.length > 0) {
+    req.session.errors = errors;
+    req.session.success = false;
+    return res.redirect("/domains/addDomain")
+  }
+
   Domain.find({ name : req.body.name}).limit(1).lean().exec((err, result) => {
     if(result.length === 0) {
       sslCertificate.get(req.body.name).then(certificate => {
@@ -43,6 +62,8 @@ router.post('/addDomain', (req, res, next) => {
           res.redirect("/domains");
         })
       })
+    } else {
+      res.redirect("/domains");
     }
   })
 })
